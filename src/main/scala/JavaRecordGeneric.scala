4a90789e686e55218616a83a1f2b8df8b0151319
xuwei-k <6b656e6a69@gmail.com>
2020-09-29 20:18:55 +0900

    support LabelledGeneric
    fix https://github.com/xuwei-k/shapeless-java-records/issues/1

--- src/main/scala/JavaRecordGeneric.scala
+++ src/main/scala/JavaRecordGeneric.scala
@@ -4,11 +4,41 @@ import scala.language.experimental.macros
 import scala.reflect.macros.whitebox
 
 object JavaRecordGeneric {
-  def apply[A]: Generic[A] = macro JavaRecordGeneric.impl[A]
+  implicit def javaRecordGeneric[A]: Generic[A] =
+    macro JavaRecordGeneric.genericImpl[A]
+
+  implicit def javaRecordSymbolicLabelling[A]: DefaultSymbolicLabelling[A] =
+    macro JavaRecordGeneric.defaultSymbolicLabellingImpl[A]
 }
 
-class JavaRecordGeneric(val c: whitebox.Context) extends shapeless.CaseClassMacros {
-  def impl[A: c.WeakTypeTag]: c.Tree = {
+class JavaRecordGeneric(val c: whitebox.Context) extends shapeless.CaseClassMacros with shapeless.SingletonTypeUtils {
+
+  def defaultSymbolicLabellingImpl[A](implicit tTag: c.WeakTypeTag[A]): c.Tree = {
+    import c.universe._
+    val tpe = weakTypeOf[A]
+    val name = tpe.toString
+    val clazz = Class.forName(name)
+
+    if (clazz.isRecord) {
+      val labels = clazz.getRecordComponents.map(_.getName).toList
+      val labelTypes = labels.map(SingletonSymbolType(_))
+      val labelValues = labels.map(mkSingletonSymbol)
+      val labelsType = mkHListTpe(labelTypes)
+      val labelsValue = mkHListValue(labelValues)
+
+      q"""
+      (new _root_.shapeless.DefaultSymbolicLabelling[$tpe] {
+        override type Out = $labelsType
+        override def apply(): $labelsType = $labelsValue
+      }) : _root_.shapeless.DefaultSymbolicLabelling.Aux[$tpe, $labelsType]
+    """
+    } else {
+      c.error(c.enclosingPosition, s"$name is not record")
+      q"_root_.scala.Predef.???"
+    }
+  }
+
+  def genericImpl[A: c.WeakTypeTag]: c.Tree = {
     import c.universe._
 
     case class Field(name: String, method: MethodSymbol)
--- src/test/scala/JavaRecordGenericTest.scala
+++ src/test/scala/JavaRecordGenericTest.scala
@@ -1,11 +1,15 @@
 import java.util.{List => JavaList}
 import org.junit.Test
+import shapeless.Generic
+import shapeless.LabelledGeneric
 
 class JavaRecordGenericTest {
+  private val a1 = new foo.A(2, "a", JavaList.of("b", "c"))
+
   @Test
-  def test1: Unit = {
-    val gen = JavaRecordGeneric[foo.A]
-    val a1 = new foo.A(2, "a", JavaList.of("b", "c"))
+  def genericTest1: Unit = {
+    import JavaRecordGeneric._
+    val gen = Generic[foo.A]
     val list = gen.to(a1)
     assert(list(0) == 2)
     assert(list(1) == "a")
@@ -13,6 +17,21 @@ class JavaRecordGenericTest {
     println(list)
     val a2 = gen.from(list)
     println(a2)
+    assert(a1 == a2)
+  }
+
+  @Test
+  def labelledGenericTest1: Unit = {
+    val labelledGen = {
+      import JavaRecordGeneric._
+      LabelledGeneric[foo.A]
+    }
+    val record = labelledGen.to(a1)
+    import shapeless.record._
+    assert(record(Symbol("x")) == 2)
+    assert(record(Symbol("y")) == "a")
+    assert(record(Symbol("z")) == JavaList.of("b", "c"))
+    val a2 = labelledGen.from(record)
     assert(a1 == a2)
   }
 }
