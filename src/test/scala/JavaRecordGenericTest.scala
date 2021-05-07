2de8be0dcea5b5a3db5f0770c9a88c0e46fe1b71
xuwei-k <6b656e6a69@gmail.com>
2021-05-07 16:23:31 +0900

    wip [skip ci]

--- src/test/scala/JavaRecordGenericTest.scala
+++ src/test/scala/JavaRecordGenericTest.scala
@@ -2,51 +2,36 @@ package example
 
 import java.util.{List => JavaList}
 import shapeless3.deriving.K0
-import org.junit.Test
 import scala.quoted.*
 import scala.jdk.OptionConverters._
 
 object JavaRecordGenericTest {
-  private def typed[A](a: A): Unit = ()
-
   transparent inline def javaInstance[A]: K0.CoproductGeneric[A] = ${ javaInstanceImpl[A] }
   transparent inline given javaInstanceGiven[A]: K0.CoproductGeneric[A] = ${ javaInstanceImpl[A] }
 
   def javaInstanceImpl[A](using a: Type[A], q: Quotes) = {
     import q.reflect._
-    val aa = q.reflect.TypeRepr.of[A]
-    val name = aa.show
+    val name = TypeRepr.of[A].show
     val clazz = Class.forName(name)
     val subClasses = clazz.getPermittedSubclasses.flatMap(_.describeConstable.toScala).map{ desc =>
       desc.packageName + "." + desc.displayName
     }.toList
-
-    println(subClasses)
-
-    val x = aa.typeSymbol.fullName
-    println(x)
-    def c(s: String): Type[?] = ConstantType(StringConstant(s)).asType
-
     val tupleClass = TypeRepr.typeConstructorOf(Class.forName("scala.Tuple" + subClasses.size.toString))
-    val tupleApplied = tupleClass.appliedTo(subClasses.map(x => TypeRepr.typeConstructorOf(Class.forName(x))))
+    val tupleApplied = tupleClass.appliedTo(subClasses.map(x => TypeRepr.typeConstructorOf(Class.forName(x)))).asType
 
-    c(name.split('.').last).match {
-      case '[t] =>
-        tupleApplied.asType match {
-          case '[elems] =>
-            '{
-              new scala.deriving.Mirror.Sum {
-                def ordinal(p: MirroredMonoType): Int = 0
-                type Kind = K0.type
-                type MirroredType = A
-                type MirroredElemTypes = elems
-              } : K0.CoproductGeneric[A]  {
-                type MirroredElemTypes = elems
-              }
-            }
+    tupleApplied match {
+      case '[elems] =>
+        '{
+          new scala.deriving.Mirror.Sum {
+            def ordinal(p: MirroredMonoType): Int = 0
+            type Kind = K0.type
+            type MirroredType = A
+            type MirroredElemTypes = elems
+          } : K0.CoproductGeneric[A]  {
+            type MirroredElemTypes = elems
+          }
         }
     }
   }
-
 }
 
